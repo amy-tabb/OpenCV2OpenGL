8206f1d583f0a9fda7aa36f6f7fb34a5f4bf6b5a
amy-tabb <amy.tabb@gmail.com>
2019-06-29 18:15:32 -0400

    initial commit

---
+++ Example1/src/OpenCV2OpenGL1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : OpenCV2OpenGL1.cpp
+// Author      : Amy Tabb
+// Version     :
+// Copyright   : MIT
+// Description :
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
